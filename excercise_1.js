3529db4404300fc4593236cada9264437163ed81
SKIronGhost <ed.diegofelix@gmail.com>
2020-11-05 20:31:24 -0300

    update excercise 1

--- excercise_1.js
+++ excercise_1.js
@@ -1,14 +1,18 @@
 //Dados un array y un valor Y, cuenta e imprime (print) el número de valores del array que sean mayores que Y.
 var array = [3,5,1,7];
 
-var y = 4;
 
-function printHigher (x){ 
-  for (i = 0; i<x.length; i++) {
+function printHigher(x){ 
+  var y = 4;
+  var qty = 0;
+
+  for (i= 0; i<x.length; i++) {
     if (x[i]>y) {
-      console.log (x[i])
+      qty = qty+1;
     }
+    
   }
-}
+  return qty;
+} 
 
-printHigher (array);
+console.log(printHigher(array));
